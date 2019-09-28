16a485aa6a04edf453dad3a4fa732891355ac82b
Felipe Machado <feelipe@email.com>
2019-09-27 23:25:36 -0300

    Iniciando a parte mobile com react-native

---
+++ tindev/src/pages/Login.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, StyleSheet, Image} from 'react-native';
+
+import logo from '../assets/logo.png';
+
+function Login() {
+  return (
+    <View style={styles.container}>
+      <Image source={logo} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+  },
+});
+
+export default Login;
