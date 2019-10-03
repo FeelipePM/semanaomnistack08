9bb81f56ef1a25ff45c57d053d4eca56431a556c
Felipe Machado <feelipe@email.com>
2019-10-02 23:47:13 -0300

    Instalando Socket.io

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,13 @@ const cors = require("cors");
 
 const routes = require("./routes");
 
-const server = express();
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+io.on("connection", socket => {
+  console.log("Nova conexão", socket.id);
+});
 
 mongoose.connect(
   "mongodb+srv://omnistack:omnistack@cluster0-h8qjf.mongodb.net/omnistack8?retryWrites=true&w=majority",
@@ -14,8 +20,8 @@ mongoose.connect(
   }
 );
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
 server.listen(3333);
